f7d7c88fe4ab49619a11d41d40282e7e411b32dc
frostyfx <agreteman@mbp-agreteman.local>
2017-04-25 14:28:17 -0700

    moving pictures around

---
+++ SkellySwift/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  SkellySwift
+//
+//  Created by Andrew Greteman on 4/25/17.
+//  Copyright © 2017 frostyFX. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITabBarControllerDelegate {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
